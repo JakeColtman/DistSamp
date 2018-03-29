42bdb45cec8e6df9740efa585691697bb9da89fb
Jake Coltman <jakecoltman@sky.com>
2018-03-29 06:22:40 +0100

    Move serialization and resoration into first class methods

--- distsamp/model/api/spark.py
+++ distsamp/model/api/spark.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 import redis
+from typing import Mapping
 
+from distsamp.distributions.state import State
 from distsamp.server.api.spark import get_server_api
 from distsamp.worker.api.spark import get_worker_api
 
@@ -20,3 +22,29 @@ def unregister_model(model_name):
     r = redis.StrictRedis(connection_pool=POOL)
     for key in r.scan_iter("{}:*".format(model_name)):
         r.delete(key)
+
+
+def serialize_model(m_api: ModelAPI) -> Mapping[str, State]:
+    output = dict()
+    output["shared"] = m_api.server_api.get_shared_state()
+    for w_id in m_api.worker_apis:
+        output[w_id] = m_api.worker_apis[w_id].get_worker_cavity()
+    return output
+
+
+def restore_model(model_name: str, serialized_model: Mapping[str, State]) -> ModelAPI:
+    from distsamp.worker.api.spark import register_named_worker
+    from distsamp.server.api.spark import get_server_api
+
+    s_api = get_server_api(model_name)
+
+    w_apis = {}
+
+    for key, value in serialized_model.items():
+        if key == "shared":
+            s_api.set_shared_state(value)
+        else:
+            w_apis[key] = register_named_worker(model_name, key)
+            w_apis[key].set_worker_state(value)
+
+    return s_api, w_apis
