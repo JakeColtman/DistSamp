e4abf3abc7b8f15bc31c96f3d3764e27a663f2c4
Jake Coltman <jakecoltman@sky.com>
2018-05-13 15:00:26 -0500

    Refactor distributions into their own classes and add guards against combining different distributions

--- distsamp/distributions/distribution.py
+++ distsamp/distributions/distribution.py
@@ -10,12 +10,18 @@ class Distribution:
         self.eta, self.llambda = eta, llambda
 
     def __truediv__(self, other: 'Distribution'):
+        if self.family != other.family:
+            raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return Distribution(self.family, self.eta - other.eta, self.llambda - other.llambda)
 
     def __mul__(self, other: 'Distribution'):
+        if self.family != other.family:
+            raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return Distribution(self.family, self.eta + other.eta, self.llambda + other.llambda)
 
     def __eq__(self, other: 'Distribution'):
+        if self.family != other.family:
+            raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return np.all(self.eta == other.eta) and np.all(self.llambda == other.llambda)
 
     def serialize(self):
@@ -25,36 +31,81 @@ class Distribution:
         return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
 
 
+class GammaDistribution(Distribution):
+
+    def __init__(self, eta, llambda):
+        self.family = "gamma"
+        self.eta, self.llambda = eta, llambda
+
+    def serialize(self):
+        return pickle.dumps(self)
+
+    def to_dict(self):
+        return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
+
+
+class GaussianDistribution(Distribution):
+
+    def __init__(self, eta, llambda):
+        self.family = "gaussian"
+        self.eta, self.llambda = eta, llambda
+
+    def serialize(self):
+        return pickle.dumps(self)
+
+    def to_dict(self):
+        return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
+
+    def expectation_parameters(self):
+        variance = np.linalg.inv(self.llambda)
+        mean = np.dot(variance, self.eta)
+        return {"mean": mean, "variance": variance}
+
+    @staticmethod
+    def from_expectation_parameters(mean: float, variance: float) -> 'GaussianDistribution':
+        llambda = np.linalg.inv(variance)
+        eta = np.dot(llambda, mean)
+        return GaussianDistribution(eta, llambda)
+
+
+class MultivariateGaussianDistribution(Distribution):
+
+    def __init__(self, eta, llambda):
+        self.family = "multivariate_gaussian"
+        self.eta, self.llambda = eta, llambda
+
+    def serialize(self):
+        return pickle.dumps(self)
+
+    def to_dict(self):
+        return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
+
+    def expectation_parameters(self):
+        variance = np.linalg.inv(self.llambda)
+        mean = np.dot(variance, self.eta)
+        return {"mean": mean, "variance": variance}
+
+    @staticmethod
+    def from_expectation_parameters(mean: np.ndarray, covariance: np.ndarray) -> 'MultivariateGaussianDistribution':
+        llambda = np.linalg.inv(covariance)
+        eta = np.dot(llambda, mean)
+        return MultivariateGaussianDistribution(eta, llambda)
+
+
 def deserialize_distribution(serialization: bytes) -> Distribution:
     return pickle.loads(serialization)
 
 
-def gaussian_convert_to_natural_parameters(mean: np.ndarray, variance: np.ndarray):
-    llambda = np.linalg.inv(variance)
-    eta = np.dot(llambda, mean)
-    return eta, llambda
+def gaussian_distribution_from_samples(samples: np.ndarray) -> GaussianDistribution:
+    mean = np.mean(samples).reshape(1,)
+    variance = np.var(samples).reshape(1, 1)
+    return GaussianDistribution.from_expectation_parameters(mean, variance)
 
 
-def gaussian_convert_to_expectation_parameters(eta: np.ndarray, llambda: np.ndarray):
-    variance = np.linalg.inv(llambda)
-    mean = np.dot(variance, eta)
-    return mean, variance
-
-
-def gaussian_distribution_from_samples(samples: np.ndarray) -> Distribution:
-    if len(samples.shape) == 1:
-        mean = np.mean(samples).reshape(1,)
-        covar = np.var(samples).reshape(1, 1)
-    else:
-        mean = np.mean(samples, axis=0)
-        covar = np.cov(samples, rowvar=False).reshape((mean.shape[0], mean.shape[0]))
-    eta, llambda = gaussian_convert_to_natural_parameters(mean, covar)
-    return Distribution("gaussian", eta, llambda)
-
-
-def gaussian_distribution_from_expectation_parameters(mean, covar) -> Distribution:
-    eta, llambda = gaussian_convert_to_natural_parameters(mean, covar)
-    return Distribution("gaussian", eta, llambda)
+def multivariate_gaussian_distribution_from_samples(samples: np.ndarray) -> MultivariateGaussianDistribution:
+    mean = np.mean(samples, axis=0)
+    covariance = np.cov(samples, rowvar=False).reshape((mean.shape[0], mean.shape[0]))
+    return MultivariateGaussianDistribution.from_expectation_parameters(mean, covariance)
 
 
 def gamma_natural_parameters_from_samples(samples):
