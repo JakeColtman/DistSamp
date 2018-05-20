635c3cae061a2c3e99af3c0bbfac9b1d7d291b9c
Jake Coltman <jakecoltman@sky.com>
2018-05-20 11:20:58 -0500

    Add typing

--- distsamp/distributions/gaussian.py
+++ distsamp/distributions/gaussian.py
@@ -7,7 +7,7 @@ from distsamp.distributions.distribution import Distribution
 
 class GaussianDistribution(Distribution):
 
-    def __init__(self, eta, llambda):
+    def __init__(self, eta: float, llambda: float):
         self.family = "gaussian"
         self.eta, self.llambda = eta, llambda
         self.mu, self.var = None, None
@@ -15,22 +15,22 @@ class GaussianDistribution(Distribution):
     def to_dict(self):
         return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
 
-    def distance_from(self, other):
+    def distance_from(self, other: 'GaussianDistribution') -> float:
         return abs(self.mean - other.mean)
 
     @property
-    def mean(self):
+    def mean(self) -> float:
         if self.mu is None:
             self.set_expectation_parameters()
         return self.mu
 
     @property
-    def variance(self):
+    def variance(self) -> float:
         if self.var is None:
             self.set_expectation_parameters()
         return self.var
 
-    def set_expectation_parameters(self):
+    def set_expectation_parameters(self) -> None:
         self.var = 1.0 / self.llambda
         self.mu = self.var * self.eta
 
@@ -47,21 +47,21 @@ class GaussianDistribution(Distribution):
         return GaussianDistribution.from_expectation_parameters(mean, variance)
 
     @staticmethod
-    def from_scipy(scipy_distribution):
+    def from_scipy(scipy_distribution) -> 'GaussianDistribution':
         mean, sigma = scipy_distribution.args
         return GaussianDistribution.from_expectation_parameters(mean, np.power(sigma, 2))
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'GaussianDistribution') -> bool:
         if self.family != other.family:
             raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return self.eta == other.eta and self.llambda == other.llambda
 
-    def __truediv__(self, other: 'GaussianDistribution'):
+    def __truediv__(self, other: 'GaussianDistribution') -> 'GaussianDistribution':
         if self.family != other.family:
             raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return GaussianDistribution(self.eta - other.eta, self.llambda - other.llambda)
 
-    def __mul__(self, other: Union[float, 'GaussianDistribution']):
+    def __mul__(self, other: Union[float, 'GaussianDistribution']) -> 'GaussianDistribution':
         if type(other) == float:
             return GaussianDistribution(self.eta * other, self.llambda * other)
 
--- distsamp/distributions/multivariategaussian.py
+++ distsamp/distributions/multivariategaussian.py
@@ -7,30 +7,30 @@ from distsamp.distributions.distribution import Distribution
 
 class MultivariateGaussianDistribution(Distribution):
 
-    def __init__(self, eta, llambda):
+    def __init__(self, eta: np.ndarray, llambda: np.ndarray):
         self.family = "multivariate_gaussian"
         self.eta, self.llambda = eta, llambda
         self.mu, self.cov = None, None
 
-    def distance_from(self, other):
+    def distance_from(self, other: 'MultivariateGaussianDistribution') -> float:
         return np.power(np.sum(np.power(self.mean - other.mean, 2)), 0.5)
 
     def to_dict(self):
         return {"family": self.family, "eta": self.eta, "llambda": self.llambda}
 
     @property
-    def mean(self):
+    def mean(self) -> np.ndarray:
         if self.mu is None:
             self.set_expectation_parameters()
         return self.mu
 
     @property
-    def covariance(self):
+    def covariance(self) -> np.ndarray:
         if self.cov is None:
             self.set_expectation_parameters()
         return self.cov
 
-    def set_expectation_parameters(self):
+    def set_expectation_parameters(self) -> None:
         self.cov = np.linalg.inv(self.llambda)
         self.mu = np.dot(self.cov, self.eta)
 
@@ -50,17 +50,17 @@ class MultivariateGaussianDistribution(Distribution):
         covariance = np.cov(samples, rowvar=False).reshape((mean.shape[0], mean.shape[0]))
         return MultivariateGaussianDistribution.from_expectation_parameters(mean, covariance)
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'MultivariateGaussianDistribution') -> bool:
         if self.family != other.family:
             raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return np.all(self.eta == other.eta) and np.all(self.llambda == other.llambda)
 
-    def __truediv__(self, other: 'MultivariateGaussianDistribution'):
+    def __truediv__(self, other: 'MultivariateGaussianDistribution') -> 'MultivariateGaussianDistribution':
         if self.family != other.family:
             raise ValueError("Operations only meaningful between distributions in the same family, found {} and {}".format(self.family, other.family))
         return MultivariateGaussianDistribution(self.eta - other.eta, self.llambda - other.llambda)
 
-    def __mul__(self, other: Union[float, 'MultivariateGaussianDistribution']):
+    def __mul__(self, other: Union[float, 'MultivariateGaussianDistribution']) -> 'MultivariateGaussianDistribution':
         if type(other) == float:
             return MultivariateGaussianDistribution(self.eta * other, self.llambda * other)
 
